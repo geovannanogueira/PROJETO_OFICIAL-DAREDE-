7691d1e01aa505b81c500a547f7b685aae799d4b
geovannanogueira <geovannat.nogueira@gmail.com>
2020-07-07 14:48:13 -0300

    segundo

--- src/pagina_auditoria/App.js
+++ src/pagina_auditoria/App.js
@@ -4,7 +4,7 @@ import Axios from 'axios';
 import Nav from '../components/Nav/nav';
 
 import './App.css';
-// import logo from '../imagens/logoOficial.png';
+import logo from '../imagens/daredePNG.png';
 
 export default class App extends React.Component {
 
@@ -43,33 +43,91 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <div className="tela_servicos">
-                <header className="container">
-                    {/* <img src={logo} className="logo" /> */}
-                    <Nav/>
-                    <div id="titulo">
-                        {/* <h1 className="classeTitle">Nossos Serviços</h1> */}
+            // <div className="tela_servicos">
+            //     <header className="container">
+            //         {/* <img src={logo} className="logo" /> */}
+            //         <Nav/>
+            //         <div id="titulo">
+            //             {/* <h1 className="classeTitle">Nossos Serviços</h1> */}
+            //         </div>
+            //         {/* <input 
+            //         // onChange={resposta => this.setState({ resposta })} value={this.state.resposta}
+            //         /> */}
+            //         {/* <button 
+            //         // onPress={this.listarServicos}
+            //         >Buscar
+            //         </button> */}
+            //         <div id="caracteristicas">
+            //             <div id="nome">
+            //                 {/* <p className="nome"><strong>Nome do serviço:</strong></p> */}
+            //             </div>
+            //             <div id="descricao">
+            //                 {/* <p className="descricao"><strong>Descrição:</strong></p> */}
+            //             </div>
+            //             <div id="valor">
+            //                 {/* <p className="valor"><strong>Valor:</strong></p> */}
+            //             </div>
+            //         </div>
+            //     </header>
+            // </div>
+            <header>
+                <div className="header">
+                    <div className="header__logo">
+                        <img src={logo} className="header__logo-img" />
                     </div>
-                    {/* <input 
-                    // onChange={resposta => this.setState({ resposta })} value={this.state.resposta}
-                    /> */}
-                    {/* <button 
-                    // onPress={this.listarServicos}
-                    >Buscar
-                    </button> */}
-                    <div id="caracteristicas">
-                        <div id="nome">
-                            {/* <p className="nome"><strong>Nome do serviço:</strong></p> */}
+                    <nav className="header__nav">
+                        <a href="#" className="header__nav-link">Serviços</a>
+                        <a href="#" className="header__nav-link">Clientes</a>
+                    </nav>
+                </div>
+                <div className="clientes">
+                    <section className="clientes__content">
+                        <h3 className="clientes__content-title">
+                            Nossos clientes
+                        </h3>
+                        <div className="clientes__content-items">
+                            <div className="clientes_content-card">
+                                <p className="text-right">26/06/2000</p>
+                                <p><b>Usuário:</b>Roberto</p>
+                                <p><b>Empresa:</b>ViaCloud</p>
+                                <p><b>Serviço:</b>Lambda</p>
+                                <p className="text-green text-right">R$150,00</p>
+                            </div>
+                            <div className="clientes_content-card">
+                                <p className="text-right">26/06/2000</p>
+                                <p><b>Usuário:</b>Roberto</p>
+                                <p><b>Empresa:</b>ViaCloud</p>
+                                <p><b>Serviço:</b>Lambda</p>
+                                <p className="text-green text-right">R$150,00</p>
+                            </div>
+                            <div className="clientes_content-card">
+                                <p className="text-right">26/06/2000</p>
+                                <p><b>Usuário:</b>Roberto</p>
+                                <p><b>Empresa:</b>ViaCloud</p>
+                                <p><b>Serviço:</b>Lambda</p>
+                                <p className="text-green text-right">R$150,00</p>
+                            </div>
                         </div>
-                        <div id="descricao">
-                            {/* <p className="descricao"><strong>Descrição:</strong></p> */}
+                    </section>
+                    <aside className="clientes__filtro">
+                        <div className="filter-box">
+                            <h5>Filtrar</h5>
+                            <div className="input-search">
+                                <input type="text" placeholder="Pesquisar..." />
+                                <i className="fas fa-search"></i>
+                            </div>
+                            <div class="metade">
+                                <input type="text" />
+                                <input type="text" />
+                            </div>
+                            <div class="metade">
+                                <input type="text" />
+                                <input type="text" />
+                            </div>
                         </div>
-                        <div id="valor">
-                            {/* <p className="valor"><strong>Valor:</strong></p> */}
-                        </div>
-                    </div>
-                </header>
-            </div>
+                    </aside>
+                </div>
+            </header>
         );
     }
 }
--- src/pagina_servicos/App.js
+++ src/pagina_servicos/App.js
@@ -4,7 +4,9 @@ import './App.css';
 import Axios from 'axios';
 import { Link } from 'react-router-dom';
 
-import logo2 from '../imagens/daredep.png';
+import logo from '../imagens/daredePNG.png';
+import lambda from '../imagens/lambda.png';
+import ec2 from '../imagens/monitoramento-cinza.png';
 
 export default class App extends React.Component {
 
@@ -17,49 +19,110 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div className="tela_home">
-        <header className="container">
-          <img src={logo2} className="logo" />
-          <div>
-            <h1>WELCOME TO OUR WEBSITE</h1>
-          </div>
+      // <div className="tela_home">
+      //   <header className="container">
+      //     <img src={logo2} className="logo" />
+      //     <div>
+      //       <h1>WELCOME TO OUR WEBSITE</h1>
+      //     </div>
 
-          {/* <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
-          <div>
-            <p>A Darede é uma empresa de consultoria especialista em serviço de TI.</p>
-          </div>
-          <div id="botoes">
-            <div className="botao1">
-              <button className="btn_ec2">
-                <strong>
-                  <Link className="EMILIA" to='/servicos'>CRIAR</Link>
-            {/* <br />
-                EC2 */}
-                </strong>
-              </button>
-              <p className="nome"><strong>EC2</strong></p>
+      //     {/* <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a> */}
+      //     <div>
+      //       <p>A Darede é uma empresa de consultoria especialista em serviço de TI.</p>
+      //     </div>
+      //     <div id="botoes">
+      //       <div className="botao1">
+      //         <button className="btn_ec2">
+      //           <strong>
+      //             <Link className="EMILIA" to='/servicos'>CRIAR</Link>
+      //       {/* <br />
+      //           EC2 */}
+      //           </strong>
+      //         </button>
+      //         <p className="nome"><strong>EC2</strong></p>
+      //       </div>
+      //       <div className="botao2">
+      //         <button className="btn_lambda">
+      //           <strong>
+      //             <Link className="EMILIA" to='/servicos'>CRIAR</Link>
+      //       {/* <br />
+      //         LAMBDA */}
+      //           </strong>
+      //         </button>
+      //         <p className="nome"><strong>LAMBDA</strong></p>
+      //       </div>
+      //     </div>
+      //   </header>
+      // </div>
+      <header className="">
+      
+      <div className="header">
+        <div className="header__logo">
+          <img src={logo} alt="" className="header__logo-img" />
+        </div>
+        <nav className="header__nav">
+          <a href="#" className="header__nav-link">Serviços</a>
+          <a href="#" className="header__nav-link">Clientes</a>
+        </nav>
+        
+        </div>
+        <div className="servicos">
+          <section className="servicos__content">
+            <h3 className="servicos__content-title">
+              Nossos serviços
+            </h3>
+            <div className="servicos__content-items">
+              <div className="servicos_content-card">
+                <div className="card-front">
+                  <img src="https://i.pinimg.com/originals/ee/bf/a4/eebfa4c693239be5b510fcc59136f608.jpg" alt="" class="card-img" />
+                  <div className="card__content">
+                    <p>Integração de On-promise para cloud</p>
+                  </div>
+                </div>
+                <div className="card-back">
+                  <h3>Integração de On-promise para cloud</h3>
+                  <br/>
+                  <p>Realize a conexão de sua infraestrutura local com seus serviços ativos na nuvem.</p>
+                </div>
+              </div>
+              <div className="servicos_content-card">
+                <div className="card-front">
+                  <img src={ec2} />
+                  <div className="card__content">
+                    <p>EC2</p>
+                  </div>
+                </div>
+                <div className="card-back">
+                  <h3>EC2</h3>
+                  <br/>
+                  <p>Alugue uma instância para criar ambientes inteiros na nuvem e configura-los com o mínimo de esforço.</p>
+                </div>
+              </div>
+              <div className="servicos_content-card">
+                <div className="card-front">
+                  <img src={lambda}/>
+                  <div className="card__content">
+                    <p>Lambda</p>
+                  </div>
+                </div>
+                <div className="card-back">
+                  <h3>Lambda</h3>
+                  <br/>
+                  <p>Este serviço fornece a criação de uma instância serveless auto gerenciável para inserir suas aplicações.</p>
+                </div>
+              </div>
             </div>
-            <div className="botao2">
-              <button className="btn_lambda">
-                <strong>
-                  <Link className="EMILIA" to='/servicos'>CRIAR</Link>
-            {/* <br />
-              LAMBDA */}
-                </strong>
-              </button>
-              <p className="nome"><strong>LAMBDA</strong></p>
-            </div>
-          </div>
-        </header>
-      </div>
+          </section>
+        </div>
+        
+      </header>
     );
   }
 }
-
